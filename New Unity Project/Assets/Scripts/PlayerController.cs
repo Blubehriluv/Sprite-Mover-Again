363e72002c208f35b9b153754567be9d80a7bb96
Taylor McKenzie <taymcken@uat.edu>
2019-03-18 23:08:32 -0700

    Camera and Player Controller Movement Fixed
    The bug turns to the direction that the player presses

--- New Unity Project/Assets/Scripts/PlayerController.cs
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -1,34 +1,148 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour {
+public class PlayerController : MonoBehaviour
+{
+    private Boolean up = false;
+    private Boolean down = true;
+    private Boolean left = false;
+    private Boolean right = false;
+
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+	{
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	    if (Input.GetKey(KeyCode.W))
+
+        //FOR THE USER TO MOVE UP
+	    if (Input.GetKeyDown(KeyCode.W))
 	    {
-	        transform.Rotate(0, 0, 0);
+	        if (down == true)
+	        {
+	            transform.Rotate(0, 0, -180);
+	            up = true;
+	            down = false;
+	            left = false;
+	            right = false;
+	        }
+	        if (left == true)
+	        {
+	            transform.Rotate(0, 0, -90);
+	            up = true;
+	            down = false;
+	            left = false;
+	            right = false;
+	        }
+
+	        if (right == true)
+	        {
+	            transform.Rotate(0, 0, 90);
+	            up = true;
+	            down = false;
+	            left = false;
+	            right = false;
+	        }
         }
 
-	    if (Input.GetKey(KeyCode.A))
+        //FOR THE USER TO MOVE DOWN
+	    if (Input.GetKeyDown(KeyCode.S))
 	    {
-	        transform.Rotate(0, 0, 90);
+	        if (up == true)
+	        {
+                transform.Rotate(0, 0, -180);
+	            up = false;
+	            down = true;
+	            left = false;
+	            right = false;
+	        }
+	        if (left == true)
+	        {
+	            transform.Rotate(0, 0, 90);
+	            up = false;
+	            down = true;
+	            left = false;
+	            right = false;
+	        }
+
+	        if (right == true)
+	        {
+	            up = false;
+	            down = true;
+	            left = false;
+	            right = false;
+	            transform.Rotate(0, 0, -90);
+	        }
+
         }
 
-	    if (Input.GetKey(KeyCode.S))
+        //FOR THE USER TO MOVE LEFT
+	    if (Input.GetKeyDown(KeyCode.A))
 	    {
-	        transform.Rotate(0, 0, -180);
-        }
+	        if (down == true)
+	        {
+                transform.Rotate(0, 0, -90);
+	            up = false;
+	            down = false;
+	            left = true;
+	            right = false;
+	        }
 
-	    if (Input.GetKey(KeyCode.D))
+	        if (up == true)
+	        {
+	            transform.Rotate(0, 0, 90);
+	            up = false;
+	            down = false;
+	            left = true;
+	            right = false;
+	        }
+
+	        if (right == true)
+	        {
+	            transform.Rotate(0, 0, -180);
+	            up = false;
+	            down = false;
+	            left = true;
+	            right = false;
+                
+	        }
+	    }
+
+        //FOR THE USER TO MOVE RIGHT
+	    if (Input.GetKeyDown(KeyCode.D))
 	    {
-	        transform.Rotate(0, 0, -90);
+	        if (down == true)
+	        {
+	            transform.Rotate(0, 0, 90);
+	            up = false;
+	            down = false;
+	            left = false;
+	            right = true;
+	        }
+
+	        if (up == true)
+	        {
+	            transform.Rotate(0, 0, -90);
+	            up = false;
+	            down = false;
+	            left = false;
+	            right = true;
+	        }
+
+	        if (left == true)
+	        {
+	            transform.Rotate(0, 0, -180);
+	            up = false;
+	            down = false;
+	            left = false;
+	            right = true;
+	            
+	        }
         }
     }
 }
