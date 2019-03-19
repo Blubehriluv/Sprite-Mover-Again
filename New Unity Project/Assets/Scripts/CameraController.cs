568b6ba5dc87d2040dd5d8cf41d76c2ecac3e079
Taylor McKenzie <taymcken@uat.edu>
2019-03-18 20:07:11 -0700

    Made Some Scripts
    Movement and Camera Controller Script. The bug now spins.

---
+++ New Unity Project/Assets/Scripts/CameraController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private Transform tf;
+    public float xAngle, yAngle, zAngle;
+    public float speed;
+
+	// Use this for initialization
+	void Start () {
+		tf = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        tf.position = tf.position + Vector3.up * speed;
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        tf.position = tf.position + Vector3.left * speed;
+	    }
+
+	    if (Input.GetKey(KeyCode.S))
+	    {
+	        tf.position = tf.position + Vector3.down * speed;
+	    }
+
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        tf.position = tf.position + Vector3.right * speed;
+	    }
+
+    }
+}
---
+++ New Unity Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKey(KeyCode.W))
+	    {
+	        transform.Rotate(0, 0, 0);
+        }
+
+	    if (Input.GetKey(KeyCode.A))
+	    {
+	        transform.Rotate(0, 0, 90);
+        }
+
+	    if (Input.GetKey(KeyCode.S))
+	    {
+	        transform.Rotate(0, 0, -180);
+        }
+
+	    if (Input.GetKey(KeyCode.D))
+	    {
+	        transform.Rotate(0, 0, -90);
+        }
+    }
+}
