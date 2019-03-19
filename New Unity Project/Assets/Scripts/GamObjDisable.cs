1994a9650791c9b2b355328e48bdc0d45837135d
Taylor McKenzie <taymcken@uat.edu>
2019-03-19 03:51:25 -0700

    Tiny Changes, Attempting the Disable
    h e l p

---
+++ New Unity Project/Assets/Scripts/GamObjDisable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamObjDisable : MonoBehaviour
+{
+
+    //public GameObject noPlay;
+
+	// Use this for initialization
+	void Start ()
+	{
+	    //noPlay = GetComponent<GameObject>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown(KeyCode.Q))
+	    {
+	        var ObjectToDeactivate = GameObject.Find("Player");
+	        ObjectToDeactivate.SetActiveRecursively(false);
+        }
+    }
+}
